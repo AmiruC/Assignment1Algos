9fb86a0930a4b2392385afdba4540e99b2531ce7
AmiruC <83331140+AmiruC@users.noreply.github.com>
2022-08-10 11:18:34 +1000

    Add files via upload

---
+++ Assign1-s12345-s67890/dictionary/array_dictionary.py
@@ -0,0 +1,83 @@
+from dictionary.word_frequency import WordFrequency
+from dictionary.base_dictionary import BaseDictionary
+import bisect
+
+
+# ------------------------------------------------------------------------
+# This class is required TO BE IMPLEMENTED
+# Array-based dictionary implementation
+#
+# __author__ = 'Son Hoang Dau'
+# __copyright__ = 'Copyright 2022, RMIT University'
+# ------------------------------------------------------------------------
+
+class ArrayDictionary(BaseDictionary):
+
+    def __init__(self):
+        # TO BE IMPLEMENTED
+        pass
+
+
+    def build_dictionary(self, words_frequencies: [WordFrequency]):
+        """
+        construct the data structure to store nodes
+        @param words_frequencies: list of (word, frequency) to be stored
+        """
+        lst = len(words_frequencies)
+        for i in words_frequencies:
+            #receiving all 20 items for the datatfile(sameDataToy.txt)
+         for j in range(0, lst-i-1):
+             if (i.word > i.word):
+                # temp = words_frequencies[j]
+                # words_frequencies[j]= words_frequencies[j + 1]
+                # words_frequencies[j + 1]= temp
+
+
+        # for i in words_frequencies:
+        #     print(i.word)
+       
+        return 0 
+        # TO BE IMPLEMENTED
+
+
+    def search(self, word: str) -> int:
+        """
+        search for a word
+        @param word: the word to be searched
+        @return: frequency > 0 if found and 0 if NOT found
+        """
+        # TO BE IMPLEMENTED
+
+        return 0
+
+    def add_word_frequency(self, word_frequency: WordFrequency) -> bool:
+        """
+        add a word and its frequency to the dictionary
+        @param word_frequency: (word, frequency) to be added
+        :return: True whether succeeded, False when word is already in the dictionary
+        """
+        # TO BE IMPLEMENTED
+
+        return False
+
+    def delete_word(self, word: str) -> bool:
+        """
+        delete a word from the dictionary
+        @param word: word to be deleted
+        @return: whether succeeded, e.g. return False when point not found
+        """
+        # find the position of 'word' in the list, if exists, will be at idx-1
+        
+        # TO BE IMPLEMENTED
+        
+
+        return False
+
+
+    def autocomplete(self, prefix_word: str) -> [WordFrequency]:
+        """
+        return a list of 3 most-frequent words in the dictionary that have 'prefix_word' as a prefix
+        @param prefix_word: word to be autocompleted
+        @return: a list (could be empty) of (at most) 3 most-frequent words with prefix 'prefix_word'
+        """
+        return []
